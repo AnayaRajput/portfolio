836e05e999b6519d4dfdca8d09a00b75b8ec64a2
AnayaRajput <amnasaeed618@gmail.com>
2024-11-18 20:15:19 -0800

    update code

---
+++ lib/Controller/constants/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColor {
+  static Color greenPrimaryColor = Color(0xff00cccc);
+  static Color darkbgColor = Color(0xff004d4d);
+  static Color lightbgColor = Color(0xff0099ffff);
+}
